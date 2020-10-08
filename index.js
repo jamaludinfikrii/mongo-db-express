172734c8d4851b981c10bc38ebd0566cf2240a7a
Jamaludin Fikri <jamaludinfikrii@gmail.com>
2020-10-08 10:20:07 +0700

    crud mongo db

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const Mongo = require('mongodb')
 const app = express()
 const PORT = 2003
 const uri = "mongodb+srv://fikri:123123123@sporteens1.c0rt6.gcp.mongodb.net/toko_baju?retryWrites=true&w=majority";
-const client = new Mongo.MongoClient(uri, { useNewUrlParser: true });
+const client = new Mongo.MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true});
+
+
+app.use(express.json())
+
+
+let db;
+
+client.connect(err => {
+    if(err) throw err;
+    db = client.db("toko_baju");
+
+});
+
+function sendError (res,message) {
+    res.send({
+        error : true,
+        message
+    })
+}
+
 
 
 app.get('/' , (req,res) => {
@@ -12,17 +32,139 @@ app.get('/' , (req,res) => {
 })
 
 app.get('/users' , (req,res) => {
-
-    client.connect(err => {
-        const collection = client.db("toko_baju").collection("users");
-        collection.find({}).toArray((err,docs) => {
+    db.collection('users').find().toArray((err,data) => {
+        try {
             if(err) throw err
-            res.send(docs)
-        })
-        // perform actions on the collection object
-        client.close();
-    });
+            res.send(data)
+        } catch (error) {
+            sendError(res,error.message)
+        }
+    })
 })
+
+
+app.get('/user/:id' , (req,res) => {
+    let id = req.params.id
+    id = Mongo.ObjectID(id)
+
+    db.collection('users').find({_id : id}).toArray((err,result) =>{ 
+        try {
+            if(err) throw err
+            res.send(result[0])
+        } catch (error) {
+            sendError(res,error.message)
+        }
+    })    
+})
+
+app.get('/user', (req,res) => {
+    let name = req.query.name
+    let address = req.query.address
+
+    let filter = {}
+
+    if(name) filter.name = name.toLowerCase()
+    if(address) filter.address = address.toLowerCase()
+    db.collection('users').find(filter).toArray((err,result) => {
+        try {
+            if(err) throw err
+            res.send(result)
+        } catch (error) {
+            sendError(res,error.message)
+        }
+        
+    })
+    
+})
+
+app.post('/user',(req,res) => {
+    const data = req.body // {usernmae , password, address}
+
+    try {
+        if(!data.username) throw {message :'username cannot null'}
+        if(!data.password) throw {message :'password cannot null'}
+        if(!data.address) throw {message :'address cannot null'}
+        db.collection('users').insertOne(data , (err,result) => {
+            try {
+                if(err) throw err
+                res.send({
+                    error : false,
+                    message : "insert success with id " + result.insertedId
+                })
+            } catch (error) {
+                sendError(res,error.message)
+            }
+        })
+
+    } catch (error) {
+        sendError(res,error.message)
+    }
+})
+
+app.patch('/user/:id' , (req,res) => {
+    const id = req.params.id
+    const data = req.body
+
+    // update user set username = 'seto' and address = 'semarang' where id = 1
+    // db.coll('name').updateOne({_id : 1} , { $set : {username : "seto" , address : "Semarang"}})
+
+    try {
+        if(!id) throw {message : "id cannot null"}
+        let query = {_id : Mongo.ObjectID(id)}
+
+        let newValues = {}
+        if(data.username)  newValues.username = data.username
+        if(data.password)  newValues.password = data.password
+        if(data.address)  newValues.address = data.address
+
+        db.collection('users').updateOne(query,{$set : newValues},(err,result) => {
+            try {
+                if(err) throw err
+                if(result.matchedCount === 0) throw {message : "id not found"}
+
+                res.send({
+                    error : false,
+                    message : "Update data success"
+                })
+            } catch (error) {
+                sendError(res,error.message)
+            }
+        })
+
+
+    } catch (error) {
+        sendError(res,error.message)
+    }
+
+   
+})
+
+
+app.delete('/user/:id' , (req,res) => {
+    const id = req.params.id
+    try {
+        if(!id) throw {message : "id cannot null"}
+        let query = {_id : Mongo.ObjectID(id)}
+
+        db.collection('users').deleteOne(query,(err,result) => {
+            try {
+                if(err) throw err
+                if(result.deletedCount === 0) throw {message : "id not found"}
+                res.send({
+                    error : false,
+                    message : "delete data success"
+                })
+            } catch (error) {
+                sendError(res,error.message)
+            }
+        })
+
+    } catch (error) {
+        sendError(res,error.message)
+    }
+})
+
+
 
 app.listen(PORT , () => console.log('Server Running on port ' + PORT))
 
@@ -34,4 +176,4 @@ app.listen(PORT , () => console.log('Server Running on port ' + PORT))
 // network access (whitelist ip address) biar bisa connect
 // create collection
 // cluster -> database -> collections (table) -> data (object)(key value pair)
-// connect with express node js
+// connect with express node js (express mongodb)
